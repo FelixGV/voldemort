c1ed5be0a8d9d67d11cbe469e61181fca1262496
Felix GV <fgv@linkedin.com>
2015-04-30 17:49:40 -0700

    BnP groundwork for High Availability: - Introduction of a FailedFetchStrategy API for dealing with failed fetches. The current behavior of deleting data on the nodes that succeeded - Introduction of a FailedFetchLock API used within the HA FailedFetchStrategy for global synchronization across BnP jobs. The concrete implem - Introduction of new BuildAndPushStatus for the hooks: SWAPPED and SWAPPED_WITH_FAILURES.
    BnP clean ups:
    - Cleaning up of dead code here and there.
    - Cleaning up of abusive admin client instantiations in BnP.

--- src/java/voldemort/client/protocol/admin/AdminClient.java
+++ src/java/voldemort/client/protocol/admin/AdminClient.java
@@ -4124,6 +4124,10 @@ public class AdminClient {
             }
 
         }
+
+        public void disableStore(Integer key, String storeName) {
+            // FIXME: Implement this.
+        }
     }
 
     public class QuotaManagementOperations {
